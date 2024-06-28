727f36457de936cbd49861763de6a49ac9ad3411
Latak1a <antonioparente24@gmail.com>
2024-06-28 21:01:01 +0200

    Update bonus5.py
    bonus 5

--- bonus5.py
+++ bonus5.py
@@ -2,3 +2,11 @@
 # [numeroVittorie, numeroPareggi, numeroSconfitte]
 # con ciascun valore estratto a sorte tra 0 e 10
 # Sviluppa poi un algoritmo per ordinare queste 20 liste trovando il primo, secondo e terzo posto
+
+import random
+max = 0
+min = 0
+risultati = [[random.randint(0,10), random.randint(0,10), random.randint(0,10)] for k in range(20)]
+for i in range(3):
+    
+print(risultati)
