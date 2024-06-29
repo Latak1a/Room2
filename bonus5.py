85994f1fda64d08300949693ee113137dd059da8
Vorg85 <alessandrodarida@gmail.com>
2024-06-29 10:14:56 +0200

    Update bonus5.py

--- bonus5.py
+++ bonus5.py
@@ -3,12 +3,47 @@
 # con ciascun valore estratto a sorte tra 0 e 10
 # Sviluppa poi un algoritmo per ordinare queste 20 liste trovando il primo, secondo e terzo posto
 
-
+#Importo il modulo random
 import random
-max = 0
-min = 0
-risultati = [[random.randint(0,10), random.randint(0,10), random.randint(0,10)] for k in range(20)]
-#for i in range(3):
+
+nickname = ["a", "b", "c", "d", "e","f","g","h","i","l","m","n","o","p","q","r","s","t","u","v","z"]
+
+#variabile che crea con la list comprehension
+risultati = [[random.choice(nickname),  random.randint(0,10), random.randint(0,10), random.randint(0,10), 0] for k in range(20)]
+#for i in range(3)
+# for giocatore in righe:
+#                 if giocatore[1] > primoPosto[1]:
+#                     primoPosto = giocatore
+#                 elif giocatore[1] == primoPosto[1]:
+#                     if giocatore[3] < primoPosto[3]:
+#                         primoPosto = giocatore
+#                     elif giocatore[3] == primoPosto[3]:
+#                         if giocatore[2] < primoPosto[2]:
+#                             primoPosto = giocatore
     
-print(risultati)
+#['t', 1, 7, 9]
+for elemento in risultati:
+    elemento[4] = elemento[1]*3 + elemento[2] + (elemento[1]-elemento[3]) 
+  
+max = 0    
+podio = [[0, 0],[0, 0],[0, 0]]
+for index, elemento in enumerate(risultati):
+    
+    if elemento[4] > podio[0][1]:
+        if elemento[4] > podio[1][1]:
+            if elemento[4] > podio[2][1]:
+                podio[2][0] = index
+                podio[2][1] = elemento[4]   
+            else:
+                podio[1][0] = index
+                podio[1][1] = elemento[4]  
+        else:   
+            podio[0][0] = index
+            podio[0][1] = elemento[4] 
+  
+print(f"Statistiche partite: {risultati}")
+          
+print(f"Il Podio:\nOro: {risultati[podio[2][0]][0]} con {podio[2][1]} punti! \nArgento: {risultati[podio[1][0]][0]} con {podio[1][1]} punti!\nBronzo: {risultati[podio[0][0]][0]} con {podio[0][1]} punti!\n")
+        
+
 
