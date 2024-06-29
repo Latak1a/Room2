0b1fb43663b94453cb73d0bdc7220167fa19dae8
Vorg85 <alessandrodarida@gmail.com>
2024-06-29 09:07:23 +0200

    Update bonus5.py

--- bonus5.py
+++ bonus5.py
@@ -7,6 +7,6 @@ import random
 max = 0
 min = 0
 risultati = [[random.randint(0,10), random.randint(0,10), random.randint(0,10)] for k in range(20)]
-for i in range(3):
+#for i in range(3):
     
 print(risultati)
