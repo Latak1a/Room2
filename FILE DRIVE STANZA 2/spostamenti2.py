ba1861f566b507043a790577907ba5716c9342bf
Vorg85 <alessandrodarida@gmail.com>
2024-08-02 21:05:42 +0200

    committo

--- FILE DRIVE STANZA 2/spostamenti2.py
+++ FILE DRIVE STANZA 2/spostamenti2.py
@@ -11,89 +11,153 @@ def spostamenti(clientiInSospeso, attrazioni, ristoro):
     #mostraParco.mostraParco(attrazioni, clientiInSospeso, ristoro)
     max_ripetizioni = 10
     
-    for ripetizioni in range(max_ripetizioni):
+    for ripetizione in range(max_ripetizioni):
         
-        if len(clientiInSospeso) != 0:
-            for attrazione in attrazioni:
-                if attrazione.tempoAttesa == 0 :
-                
-                    print(f"Fine giro: {attrazione.nome}")
-                    for index, persone in enumerate(attrazione.clientiServiti):
-                                            
-                        clientiInSospeso.append(attrazione.clientiServiti[index])
-                                            
-                    attrazione.clientiServiti = []
-                    attrazione.capienzaAttuale = attrazione.capienzaMassima
+        for attrazione in attrazioni:
+            if attrazione.tempoAttesa == 0:
+                for cliente in attrazioni.clientiServiti[:]:
+                    attrazione.clientiServiti.remove(cliente)
+                    clientiInSospeso.append(cliente)
                     
-                    while attrazione.capienzaAttuale > 0 and len(attrazione.clientiInAttesa) > 0:
-                        persona = attrazione.clientiInAttesa.pop(0) # p.s: la funzione pop restituisce ciò che toglie da una lista ;)
-                        attrazione.clientiServiti.append(persona)
-                        attrazione.capienzaAttuale -= 1
+                attrazione.capienzaAttuale = attrazione.capienzaMassima
+                
+                while attrazione.capienzaAttuale > 0 and len(attrazione.clientiInAttesa) > 0:
+                    cliente = attrazione.clientiInAttesa.pop(0) # p.s: la funzione pop restituisce ciò che toglie da una lista ;)
+                    attrazione.clientiServiti.append(cliente)
+                    attrazione.capienzaAttuale -= 1
+                
+                if attrazione.capienzaAttuale == 0:
+                    attrazione.tempoAttesa = 5
+                else:
+                    attrazione.tempoAttesa -= 1
+            
+            
+            for cliente in clientiInSospeso[:]:
+                if len(cliente.attrazioniDesiderate) > 0:
+                    clientiInSospeso.remove(cliente)
+                    
+                    for attrazione in attrazioni:
+                        if attrazione.nome == cliente.attrazioniDesiderate[0]:
+                    
+                            if attrazione.capienzaAttuale > 0:
+                                attrazione.clientiServiti.append(cliente)
+                                attrazione.capienzaAttuale -= 1
+                            else:
+                                attrazione.clientiInAttesa.append(cliente)
                         
-                    if attrazione.capienzaAttuale == 0:
-                        print(f"Inizio giro: {attrazione.nome}")
-                    else:
-                        attrazione.tempoAttesa -= 1
-                        for cliente in clientiInSospeso:
-                            # print(cliente)
-                            if len(cliente.attrazioniDesiderate) > 0 or cliente.tipo != "Adulto":
-                                if attrazione.nome == cliente.attrazioniDesiderate[0]:
+                    cliente.attrazioniDesiderate.pop(0)
+            print(f"Iterazione n. {ripetizione} ---\n")
+            print(f"Clienti in Sospeso: \n{clientiInSospeso} ------------\n")
+            
+        for attrazione in attrazioni:
+            print(attrazione)
+            scelta = input(f"Premi invio per passare alla ripetizione n. {ripetizione + 1}: ")
+    
+            
+            
+        # if len(clientiInSospeso) != 0:
+            
+        #     for attrazione in attrazioni:
+                #Se il tempo di attesa o la capienzaAttuale dell'attrazione sono pari a 0 la giostra finisce il suo giro
+                #Quindi svuoto i clienti serviti e li riporto ai clienti in sospeso
+                # if attrazione.tempoAttesa == 0:
+                #     print(f"{attrazione.tempoAttesa =} ")
+                #     print(f"Fine giro: {attrazione.nome}")
+                #     if attrazione.capienzaAttuale == 0:
+                #         for index, clienti in enumerate(attrazione.clientiServiti):
+                                        
+                #             clientiInSospeso.append(attrazione.clientiServiti[index].pop())
+                #             print(clientiInSospeso)
+                #             print(attrazione.clientiServiti)
+                                            
+                    
+                #         attrazione.capienzaAttuale = attrazione.capienzaMassima
+                #         attrazione.tempoAttesa = 5
+                    
+                #         while attrazione.capienzaAttuale > 0 and len(attrazione.clientiInAttesa) > 0:
+                            
+                #             cliente = attrazione.clientiInAttesa.pop(0) # p.s: la funzione pop restituisce ciò che toglie da una lista ;)
+                #             attrazione.clientiServiti.append(cliente)
+                #             attrazione.capienzaAttuale -= 1
+                #             attrazione.tempoAttesa -= 1
+                #     else:
+                #         attrazione.tempoAttesa -= 1
+                            
+                #     print(f"Inizio giro: {attrazione.nome}")
+                        
+                
+                            
+                            
+                        
+                        # if attrazione.nome == cliente.attrazioniDesiderate[0]:
+                        #     if len(cliente.attrazioniDesiderate) > 0 or cliente.tipo != "Adulto":
+                        #         persona = clientiInSospeso.pop(0)
+                            
+                        #         attrazione.clientiInAttesa.append(persona)
+                        #         cliente.attrazioniDesiderate.pop(0)
+                        #         attrazione.tempoAttesa -= 1
+                        #     else:
+                        #         print("ciao")
+                    
+        #         print(f"\n{attrazione}")
+                
+        # print(input("Premi invio per continuare")) 
                                     
                        
                                 
                                
                                     
-                                        attrazione.clientiServiti.append(cliente)
-                                        attrazione.capienzaAttuale -= 1
-                                        clientiInSospeso.remove(cliente)
+                    #                     attrazione.clientiServiti.append(cliente)
+                    #                     attrazione.capienzaAttuale -= 1
+                    #                     clientiInSospeso.remove(cliente)
                                         
-                                        #Sposta la posizione del cliente
-                                        cliente.posizione[0] = attrazione.posizione[0] + 5
-                                        cliente.posizione[1] = attrazione.posizione[1] + 5
-                                        #print(cliente)
-                                        attrazione.tempoAttesa -= 1
-                                        print(f"1   {attrazione.tempoAttesa =} {attrazione.nome}")
+                    #                     #Sposta la posizione del cliente
+                    #                     cliente.posizione[0] = attrazione.posizione[0] + 5
+                    #                     cliente.posizione[1] = attrazione.posizione[1] + 5
+                    #                     #print(cliente)
+                    #                     attrazione.tempoAttesa -= 1
+                    #                     print(f"1   {attrazione.tempoAttesa =} {attrazione.nome}")
                                         
-                                else:
-                                    #GIOSTRA PIENA
-                                    attrazione.clientiInAttesa.append(cliente)
-                                    clientiInSospeso.remove(cliente)
+                    #             else:
+                    #                 #GIOSTRA PIENA
+                    #                 attrazione.clientiInAttesa.append(cliente)
+                    #                 clientiInSospeso.remove(cliente)
                                     
-                                    #SpOSTARE LA POSIZIONE DEL CLIENTE
-                                    cliente.posizione[0] = attrazione.posizione[0] + 5
-                                    cliente.posizione[1] = attrazione.posizione[1] + 5
-                                    cliente.attrazioniDesiderate.pop(0)
-                                    #print(cliente)
+                    #                 #SpOSTARE LA POSIZIONE DEL CLIENTE
+                    #                 cliente.posizione[0] = attrazione.posizione[0] + 5
+                    #                 cliente.posizione[1] = attrazione.posizione[1] + 5
+                    #                 cliente.attrazioniDesiderate.pop(0)
+                    #                 #print(cliente)
                                     
-                                    print(f"2   {attrazione.tempoAttesa =} {attrazione.nome}")
+                    #                 print(f"2   {attrazione.tempoAttesa =} {attrazione.nome}")
                                     
-                                    if attrazione.tempoAttesa == 0 :
+                    #                 if attrazione.tempoAttesa == 0 :
                                         
-                                        print(f"Inizio giro: {attrazione.nome}")
-                                        print(f"Fine giro: {attrazione.nome}")
-                                        
-                                        
+                    #                     print(f"Inizio giro: {attrazione.nome}")
+                    #                     print(f"Fine giro: {attrazione.nome}")
                                         
                                         
-                                        attrazione.tempoAttesa = 5    
-                                    elif attrazione.tempoAttesa != 0 and len(attrazione.clientiInAttesa) == 0:
-                                        attrazione.tempoAttesa -= 1
+                                        
+                                        
+                    #                     attrazione.tempoAttesa = 5    
+                    #                 elif attrazione.tempoAttesa != 0 and len(attrazione.clientiInAttesa) == 0:
+                    #                     attrazione.tempoAttesa -= 1
                                     
-                    else:
-                        clientiInSospeso.remove(cliente)
-                        ristoro.clientiRistoro.append(cliente)
+                    # else:
+                    #     clientiInSospeso.remove(cliente)
+                    #     ristoro.clientiRistoro.append(cliente)
                         
-                        #SOSTARE LA POSIZIONE DEL CLIENTE
-                        cliente.posizione[0] = ristoro.posizione[0] + 5
-                        cliente.posizione[1] = ristoro.posizione[1] + 5
-                        #print(cliente)
+                    #     #SOSTARE LA POSIZIONE DEL CLIENTE
+                    #     cliente.posizione[0] = ristoro.posizione[0] + 5
+                    #     cliente.posizione[1] = ristoro.posizione[1] + 5
+                    #     #print(cliente)
                        
                         
-                print(f"\n{attrazione}")
+                # 
                     
         
                  
-        print(input("Premi invio per continuare"))                    
+                       
     #mostraParco.mostraParco(attrazioni, clientiInSospeso, ristoro)
     
         
